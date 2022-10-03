4bb7de8bb53cc088df493b6242b6bac6d912c470
Hawder Veiga <70068618+prog-hawder@users.noreply.github.com>
2022-10-03 17:43:16 -0300

    app cru

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_notes/screens/note_editor.dart';
+import 'package:flutter_notes/screens/note_reader.dart';
+import 'package:flutter_notes/widgets/note_card.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../style/app_style.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -11,7 +18,71 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: AppStyle.mainColor,
+      appBar: AppBar(
+          elevation: 0.0,
+          title: Text("Notes"),
+          centerTitle: true,
+          backgroundColor: AppStyle.mainColor),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Notas recentes",
+              style: GoogleFonts.roboto(
+                color: Colors.white,
+                fontWeight: FontWeight.w300,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream:
+                    FirebaseFirestore.instance().collection("Notes").snapshots(),
+                builder: (context, AsyncSnapshot <QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    return GridView(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                      children: snapshot.data!.docs
+                          .map((note)=> noteCard((){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => 
+                                  NoteReaderScreen(note)));
+                          }, note))
+                          .toList(),
+                    );
+                  }
+                  return Text("ther's no Notes", style: GoogleFonts.nunito(color: Colors.white) ,);
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => 
+                NoteEditorScreen()));
+        }, 
+        label: Icon(Icons.add_rounded),  
+        ),
     );
   }
 }
--- lib/style/app_style.dart
+++ lib/style/app_style.dart
@@ -4,11 +4,10 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AppStyle {
   static Color bgColor = Colors.white;
-  static Color mainColor = Color(0xFF000633);
+  static Color mainColor = Color(0xFF3e3e3f);
   static Color accentColor = Color(0xFF0065FF);
 
   static List<Color> cardsColor = [
-    Colors.white,
     Colors.red.shade100,
     Colors.pink.shade100,
     Colors.orange.shade100,
@@ -21,6 +20,9 @@ class AppStyle {
   static TextStyle mainTitle =
       GoogleFonts.roboto(fontSize: 18.0, fontWeight: FontWeight.bold);
 
-  static TextStyle mainTitle =
-      GoogleFonts.roboto(fontSize: 18.0, fontWeight: FontWeight.bold);
+  static TextStyle mainContent =
+      GoogleFonts.nunito(fontSize: 16.0, fontWeight: FontWeight.normal);
+  
+  static TextStyle dateTitle =
+      GoogleFonts.nunito(fontSize: 13.0, fontWeight: FontWeight.w500);
 }
